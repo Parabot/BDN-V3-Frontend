f5cc74eebc12445e78bb7a4ea39a622eda5914a2
JKetelaar <jeroen@ketelaar.me>
2016-07-03 18:24:35 +0200

    [FEATURE] Added header

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,42 @@
-var websiteApp = angular.module('websiteApp', []);
+var websiteModule = angular.module('websiteModule', []);
+websiteModule.controller('homeCtrl', function () {
 
-websiteApp.controller('homeCtrl', function (){
-    
 });
+
+var headerModule = angular.module('headerModule', []);
+headerModule.controller('headerCtrl',
+    function ($scope) {
+        $scope.cart_items = [
+            {
+                name: 'Product 1',
+                price: 50,
+                image: 'assets/images/img-one.jpg'
+            },
+            {
+                name: 'Product 2',
+                price: 20,
+                image: 'assets/images/img-two.jpg'
+            },
+            {
+                name: 'Product 3',
+                price: 180,
+                image: 'assets/images/img-three.jpg'
+            }
+        ];
+
+        $scope.cart_items_amount = 0;
+        for (var key in $scope.cart_items) {
+            if ($scope.cart_items.hasOwnProperty(key)) {
+                var item = $scope.cart_items[key];
+                $scope.cart_items_amount += item.price;
+            }
+        }
+        $scope.cart_items_amount.toFixed(2);
+
+        $scope.remove = function (index) {
+            $scope.items.splice(index, 1);
+        }
+    }
+);
+
+var websiteApp = angular.module('websiteApp', ['headerModule', 'websiteModule']);
