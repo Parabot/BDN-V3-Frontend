96bf894e20540812afb43c6b68670f80b0a29552
JKetelaar <jeroen@ketelaar.me>
2016-11-09 17:27:04 +0100

    [FEATURE] Moved to a new theme

--- gulpfile.js
+++ gulpfile.js
@@ -1,108 +1,262 @@
-var gulp = require('gulp'),
-    bower = require('gulp-bower'),
-    browserSync = require( 'browser-sync' ).create(),
-    concat = require('gulp-concat'),
-    jshint = require('gulp-jshint'),
-    notify = require('gulp-notify'),
-    plugins = require('gulp-load-plugins')({
-        camelize: true
-    }),
-    sourcemaps = require('gulp-sourcemaps'),
-    minifyCSS = require('gulp-minify-css'),
-    rename = require('gulp-rename');
+var gulp = require('gulp');
+var args = require('yargs').argv;
+var browserSync = require('browser-sync');
+var config = require('./gulp.config')();
+var del = require('del');
+var $ = require('gulp-load-plugins')({lazy: true});
 
-var config = {
-    urls: {
-        production: 'http://beta.bdn.parabot.org/',
-        development: 'http://beta.bdn.parabot.org:88/'
+gulp.task('help', $.taskListing);
+gulp.task('default', ['help']);
+
+gulp.task('vet', function() {
+    log('Analyzing source with JSHint and JSCS');
+
+    return gulp
+        .src(config.alljs)
+        .pipe($.if(args.verbose, $.print()))
+        .pipe($.jscs())
+        .pipe($.jshint())
+        .pipe($.jshint.reporter('jshint-stylish', {verbose: true}))
+        .pipe($.jshint.reporter('fail'));
+});
+
+gulp.task('clean-tmp', function(done) {
+    var files = config.tmp;
+    clean(files, done);
+});
+
+gulp.task('clean', function(done) {
+    var delconfig = [].concat(config.dist, config.tmp);
+    log('Cleaning ' + $.util.colors.blue(delconfig));
+    del(delconfig, done);
+});
+
+gulp.task('clean-all', function(done) {
+    var delconfig = config.allToClean;
+    log('Cleaning ' + $.util.colors.blue(delconfig));
+    clean(delconfig, done);
+});
+
+gulp.task('jade-docs', function() {
+    log('Compiling docs jade --> html');
+
+    var options = {
+        pretty: false
     }
-};
 
-var paths = {
-    src: {
-        css: ['./assets/css/*.css', '!./assets/css/*.min.css'],
-        fonts: './assets/fonts/*.{eot,svg,ttf,woff,woff2}',
-        images: ['./assets/images/*.{png,jpg,jpeg,gif,svg}', './assets/svg/*.{png,jpg,jpeg,gif,svg}'],
-        js: [
-            './assets/js/jquery-ui.js',
-            './assets/js/jquery.fancybox.js',
-            './assets/js/bootstrap.js',
-            './assets/js/owl.carousel.js',
-            './assets/js/jquery.revolution.js',
-            './assets/js/site.js'
-        ]
-    },
-    dist: {
-        css: './build/css',
-        fonts: './build/fonts',
-        images: './build/images',
-        js: './build/js'
+    return gulp
+        .src(config.docsJade)
+        .pipe($.plumber({errorHandler: swallowError}))
+        .pipe($.jade(options))
+        .pipe(gulp.dest(config.docs));
+});
+
+gulp.task('less', function() {
+    log('Compiling Less --> CSS');
+
+    return gulp
+        .src(config.less)
+        .pipe($.plumber({errorHandler: swallowError}))
+        .pipe($.less())
+        .pipe($.autoprefixer())
+        .pipe(gulp.dest(config.tmp));
+});
+
+gulp.task('less-watcher', function() {
+    gulp.watch([config.less], ['less']);
+});
+
+gulp.task('sass', function() {
+    log('Compiling Sass --> CSS');
+
+    var sassOptions = {
+        outputStyle: 'nested' // nested, expanded, compact, compressed
+    };
+
+    return gulp
+        .src(config.sass)
+        .pipe($.plumber({errorHandler: swallowError}))
+        .pipe($.sourcemaps.init())
+        .pipe($.sass(sassOptions))
+        .pipe($.autoprefixer())
+        .pipe($.sourcemaps.write())
+        .pipe(gulp.dest(config.tmp + '/styles'));
+});
+
+gulp.task('sass-min', function() {
+    log('Compiling Sass --> minified CSS');
+
+    var sassOptions = {
+        outputStyle: 'compressed' // nested, expanded, compact, compressed
+    };
+
+    return gulp
+        .src(config.sass)
+        .pipe($.plumber({errorHandler: swallowError}))
+        .pipe($.sass(sassOptions))
+        .pipe($.autoprefixer())
+        .pipe(gulp.dest(config.tmp + '/styles'));    
+})
+
+gulp.task('sass-watcher', function() {
+    gulp.watch([config.sass], ['sass']);
+});
+
+gulp.task('inject', function() {
+    log('Injecting custom scripts to index.html');
+
+    return gulp
+        .src(config.index)
+        .pipe( $.inject(gulp.src(config.js), {relative: true}) )
+        .pipe(gulp.dest(config.client));
+});
+
+gulp.task('copy', function() {
+    log('Copying assets');
+
+    return gulp
+        .src(config.assets, {base: config.client})
+        .pipe(gulp.dest(config.dist + '/'));
+});
+
+gulp.task('optimize', ['inject', 'sass-min'], function() {
+    log('Optimizing the js, css, html');
+
+    return gulp
+        .src(config.index)
+        .pipe($.plumber({errorHandler: swallowError}))
+        .pipe($.useref())
+        .pipe($.if('scripts/app.js', $.uglify()))
+        .pipe(gulp.dest( config.dist ));
+
+});
+
+
+gulp.task('serve', ['inject', 'sass'], function() {
+    startBrowserSync('serve');
+});
+
+gulp.task('build', ['optimize', 'copy'], function() {
+    startBrowserSync('dist');
+})
+
+gulp.task('serve-dist', function() {
+    gulp.run('build');
+})
+
+gulp.task('serve-docs', ['jade-docs'], function() {
+    startBrowserSync('docs');
+})
+
+
+
+function clean(path, done) {
+    log('Cleaning: ' + $.util.colors.blue(path));
+    del(path, done);
+}
+
+function log(msg) {
+    if (typeof(msg) === 'object') {
+        for (var item in msg) {
+            if (msg.hasOwnProperty(item)) {
+                $.util.log($.util.colors.green(msg[item]));
+            }
+        }
+    } else {
+        $.util.log($.util.colors.green(msg));
     }
-};
+}
 
-gulp.task('styles', function () {
-    return gulp.src(paths.src.css)
-        .pipe(minifyCSS())
-        .pipe(concat('styles.css'))
-        .pipe(gulp.dest(paths.dist.css))
-});
+function swallowError (error) {
+    // If you want details of the error in the console
+    console.log(error.toString());
 
-gulp.task('images', function () {
+    this.emit('end');
+}
 
-    return gulp.src(paths.src.images)
-        .pipe(plugins.cache(plugins.imagemin({
-            progressive: true,
-            interlaced: true
-        })))
-        .pipe(plugins.size({title: 'Images:'}))
-        .pipe(gulp.dest(paths.dist.images));
+function startBrowserSync(opt) {
+    if (args.nosync || browserSync.active) {
+        return;
+    }
 
-});
-
-gulp.task('fonts', function () {
-
-    return gulp.src(paths.src.fonts)
-        .pipe(gulp.dest(paths.dist.fonts));
-
-});
-
-gulp.task('scripts', function () {
-
-    return gulp.src(paths.src.js)
-        .pipe(sourcemaps.init())
-        .pipe(plugins.include())
-        .pipe(plugins.uglify({preserveComments: 'some'}))
-        .pipe(plugins.size({title: 'Javascripts: '}))
-        .pipe(concat('scripts.js'))
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest(paths.dist.js));
-
-});
-
-gulp.task( 'reload', function() {
-
-    return gulp.src( paths.dist.css, { read: false } )
-        .pipe( notify( {
-            title: config.name,
-            message: 'Compilation completed, reloading...',
-            sound: false
-        } ) );
-
-} );
-
-gulp.task( 'serve', [ 'styles', 'scripts', 'fonts', 'images' ], function() {
-
-    browserSync.init( {
-        notify: false,
+    var options = {
         port: 3000,
-        proxy: config.urls.development
-    } );
+        ghostMode: {
+            clicks: false,
+            location: false,
+            forms: false,
+            scroll: true
+        },
+        injectChanges: true,
+        logFileChanges: true,
+        logLevel: 'debug',
+        logPrefix: 'gulp-patterns',
+        notify: true,
+        reloadDelay: 0, //1000,
+        online: false
+    };
 
-    gulp.watch( [ paths.src.css ], [ 'styles', 'reload', browserSync.reload ] );
-    gulp.watch( [ paths.src.js ], [ 'scripts', 'reload', browserSync.reload ] );
-    gulp.watch( [ paths.src.fonts ], [ 'fonts', 'reload', browserSync.reload ] );
-    gulp.watch( [ paths.src.images ], [ 'images', 'reload', browserSync.reload ] );
+    switch(opt) {
+        case 'dist':
+            log('Serving dist app');
+            serveDistApp();
+            break;
+        case 'docs':
+            log('Serving docs');
+            serveDocs();
+            break;
+        default:
+            log('Serving app');
+            serveApp();
+            break;
+    }
 
-} );
+    function serveApp() {
+        gulp.watch([config.sass], ['sass']);
 
-gulp.task('assets', ['scripts', 'styles', 'fonts', 'images']);
+        options.server = {
+            baseDir: [
+                config.client,
+                config.tmp
+            ]
+        };
+        options.files = [
+            config.client + '/**/*.*',
+            '!' + config.sass,
+            config.tmp + '/**/*.css'
+        ];
+
+        browserSync(options);
+    }
+
+    function serveDistApp() {
+        options.server = {
+            baseDir: [
+                config.dist
+            ]
+        };
+        options.files = [];
+
+        browserSync(options);
+    }
+
+    function serveDocs() {
+        gulp.watch([config.docsJade], ['jade-docs']);
+
+        options.server = {
+            baseDir: [
+                config.docs
+            ]
+        }
+
+        options.files = [
+            config.docs + '/index.html',
+            '!' + config.jade
+        ];
+
+        browserSync(options);
+    }
+
+}
+
+
