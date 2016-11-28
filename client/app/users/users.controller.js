6fb4a6385b06e1de545dde335108a34a059d0e19
JKetelaar <jeroen@ketelaar.me>
2016-11-28 01:21:34 +0100

    [TASK] Removed outcomment

--- client/app/users/users.controller.js
+++ client/app/users/users.controller.js
@@ -54,7 +54,7 @@
                 $scope.signinLoading = false;
                 $scope.signinType = 'accent';
             } else {
-                // $location.url('/');
+                $location.url('/');
             }
         };
 
